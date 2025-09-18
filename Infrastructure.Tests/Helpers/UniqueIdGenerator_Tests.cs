ec0c1920c955c6241f4eb54245f8633f83073a1d
FredricDomvall <Fredric.domvall@outlook.com>
2025-09-18 11:07:18 +0200

    id test complete

---
+++ Infrastructure.Tests/Helpers/UniqueIdGenerator_Tests.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure.Helpers;
+
+namespace Infrastructure.Tests.Helpers;
+
+public class UniqueIdGenerator_Tests
+{
+    [Fact]
+    public void Generate_ShouldReturnGuidId_InStringFormat_WhenSuccessful()
+    {
+        //arrange
+
+        //act
+        var result = UniqueIdGenerator.Generate();
+        //assert
+        Assert.NotEmpty(result);
+        Assert.True(Guid.TryParse(result, out Guid id));
+        Assert.IsType<Guid>(id);
+    }
+}
