e1786050b1ac6e8d5e38d4e8e798088db425af40
FredricDomvall <Fredric.domvall@outlook.com>
2025-09-18 10:13:27 +0200

    IFileService and FileResult created

---
+++ Infrastructure/Models/FileResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Models;
+
+public class FileResult
+{
+    public bool IsSuccess { get; set; }
+    public string Response { get; set; } = null!;
+    public string? Content { get; set; }
+}
