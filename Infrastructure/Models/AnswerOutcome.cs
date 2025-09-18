121e2cceb5783a46725e1f46356526d4be0785c7
FredricDomvall <Fredric.domvall@outlook.com>
2025-09-18 09:55:47 +0200

    created responseclass

---
+++ Infrastructure/Models/AnswerOutcome.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Models;
+
+public class AnswerOutcome<T> where T : class
+{
+    public bool IsSuccess { get; set; }
+    public string Response { get; set; } = null!;
+    public T? Content { get; set; }
+}
